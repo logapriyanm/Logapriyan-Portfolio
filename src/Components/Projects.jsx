75eadeccf38685f256e24548eabcd36d45d61571
logapriyanm <logapriyanvky@gmail.com>
2025-09-17 16:02:09 +0530

    Datas ADD

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,139 +1,12 @@
 import { useState, useRef } from "react";
-
-import Html from '../assets/images/Chronoloki.png';
-import Html1 from '../assets/images/lokitech.png';
-import Javascript1 from '../assets/images/passgen.png';
-import Javascript2 from '../assets/images/Calculator.png';
-import WeatherApp from '../assets/images/WeatherApp.png';
-import Orphanage from '../assets/images/Orphnage.png';
-import BookBazaar from "../assets/images/BookBazaar.png";
-import IntegratedFarm from "../assets/images/IntegratedFarm.png";
-import CrudApp from "../assets/images/CRUDAPP.png";
-import BlogImage from "../assets/images/BlogImage.png";
-import ContactManagement from "../assets/images/ContactMERN.png"
-
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
+import ProjectsData from "../Datas/ProjectsData";
 
-// ------------------- PROJECT DATA -------------------
 
-const projects = [
-  {
-    title: "Logi Integrated Farm",
-    description:
-       " Enhanced my skills in building responsive layouts, managing state, and integrating interactive features. This project demonstrates my ability to create modern interfaces and deliver seamless user experiences for clients in the agriculture sector.",
-    image: IntegratedFarm,
-    tags: ["HTML", "Tailwind CSS", "JavaScript", "React"],
-    category: "frontend",
-    demoLink: "https://logi-integrated-farm.onrender.com",
-    codeLink: "https://github.com/logapriyanm/Integrated-Farm.git"
-  },
-  {
-  title: "Contact Management System",
-  description:
-    "Built a MERN stack contact management system with CRUD operations, search, and filtering. This project showcases my skills in full-stack development and creating efficient user interfaces.",
-  image: ContactManagement,
-  tags: [" React", "Express", "MongoDB", "Node.js", "Tailwind CSS"],
-  category: "fullstack",
-  demoLink: "https://contact-management-mern.onrender.com",
-  codeLink: "https://github.com/logapriyanm/Contact-Management-MERN.git"
-},
-
-  {
-    title: "Orphanage Management System",
-    description:
-      "A responsive React.js + Tailwind CSS web application for a non-profit organization, featuring dynamic content, an interactive donation system, volunteer sign-up, and contact management to support child welfare initiatives.",
-    image: Orphanage,
-    tags: ["HTML", "Tailwind CSS", "JavaScript", "React"],
-    category: "frontend",
-    demoLink: "https://orphanage-management-system-kxtd.onrender.com",
-    codeLink: "https://github.com/logapriyanm/Orphanage-Management-System.git"
-  },
-  {
-    title: "MERN Blog Website",
-    description:
-      "A full-stack blog platform built using MongoDB, Express.js, React, and Node.js. Features include category management, post creation, dynamic content rendering.",
-    image: BlogImage,
-    tags: ["MongoDB", "Express.js", "React", "Node.js", "REST API"],
-    category: "fullstack",
-    demoLink: "https://blog-mern-frontend-ujkd.onrender.com",
-    codeLink: "https://github.com/logapriyanm/Blog-MERN.git"
-  },
-  {
-    title: "Book Bazaar",
-    description:
-     "Developed a modern and interactive online bookstore where users can seamlessly browse, search, and discover books.  It reflects my understanding of frontend architecture, user experience design, and component-based development.",
-    image: BookBazaar,
-    tags: ["HTML", "Tailwind CSS", "JavaScript", "React"],
-    category: "frontend",
-    demoLink: "https://book-bazaar-ovgc.onrender.com",
-    codeLink: "https://github.com/logapriyanm/Book-Bazaar.git"
-  },
-  {
-    title: "Calculator",
-    description:
-      "A responsive web calculator built with Supports keyboard input, handles real-time calculations, and blocks invalid characters. Designed for clean usability and smooth interaction.",
-    image: Javascript2,
-    tags: ["HTML", "CSS", "JavaScript"],
-    category: "frontend",
-    demoLink: "https://logapriyanm.github.io/Calculator/",
-    codeLink: "https://github.com/logapriyanm/Calculator"
-  },
-  {
-    title: "User Data",
-    description:
-      "A full-stack CRUD application with React.js frontend and Node.js backend. It allows users to add, edit, delete, and search user records, with persistent data storage using JSON files.",
-    image: CrudApp,
-    tags: ["HTML", "Tailwind CSS", "JavaScript", "React", "Node js", "Express js"],
-    category: "fullstack",
-    demoLink: "https://user-data-frontend-gtb4.onrender.com/",
-    codeLink: "https://github.com/logapriyanm/User-Data.git"
-  },
-  {
-    title: "Weather App",
-    description:
-      "A responsive React-based weather application that displays real-time weather data, including temperature, humidity, wind speed, and location coordinates.",
-    image: WeatherApp,
-    tags: ["HTML", "Tailwind CSS", "JavaScript", "React"],
-    category: "frontend",
-    demoLink: "https://weather-app-loki.onrender.com",
-    codeLink: "https://github.com/logapriyanm/Weather-App-Loki.git"
-  },
-  {
-    title: "Loki Technologies",
-    description:
-      "It features clean structure, bold typography, and a futuristic tech aesthetic—designed to impress and built to perform on all devices.",
-    image: Html1,
-    tags: ["HTML", "Bootstrap"],
-    category: "frontend",
-    demoLink: "https://logapriyanm.github.io/LOKI-Technologies/",
-    codeLink: "https://github.com/logapriyanm/LOKI-Technologies"
-  },
-  {
-    title: "Watch Shop",
-    description:
-      "A sleek and modern landing page for a premium watch store, highlighting luxury, technology, and elegance. Built with HTML, CSS, and Bootstrap for a clean and engaging user experience.",
-    image: Html,
-    tags: ["HTML", "Bootstrap"],
-    category: "frontend",
-    demoLink: "https://logapriyanm.github.io/Chrono-LOGI/",
-    codeLink: "https://github.com/logapriyanm/Chrono-LOGI"
-  },
-  {
-    title: "Password Generator",
-    description:
-      "A simple yet powerful JavaScript-based password generator that lets users customize password length and include numbers, capital/small letters, and symbols.",
-    image: Javascript1,
-    tags: ["HTML", "CSS", "JavaScript"],
-    category: "frontend",
-    demoLink: "https://logapriyanm.github.io/Pass-Generator/",
-    codeLink: "https://github.com/logapriyanm/Pass-Generator.git"
-  },
-];
 
 // ------------------- PROJECT CARD -------------------
 
@@ -170,7 +43,7 @@ const ProjectCard = ({ title, description, image, tags, demoLink, codeLink }) =>
   </div>
 );
 
-// ------------------- MAIN PROJECTS COMPONENT -------------------
+
 
 const Projects = () => {
   const [selectedCategory, setSelectedCategory] = useState("all");
@@ -184,8 +57,9 @@ const Projects = () => {
 
   const filteredProjects =
     selectedCategory === "all"
-      ? projects
-      : projects.filter((project) => project.category === selectedCategory);
+      ? ProjectsData
+      : ProjectsData.filter((project) => project.category === selectedCategory);
+
 
   return (
     <div id="projects" className="min-h-screen bg-gradient-to-r from-primary to-secondary md:p-10  flex flex-col items-center">
@@ -197,9 +71,8 @@ const Projects = () => {
           <button
             key={cat}
             onClick={() => setSelectedCategory(cat)}
-            className={`px-4 py-2 rounded-md cursor-pointer transition-all duration-200 ${
-              selectedCategory === cat ? "bg-blue-500 text-white" : "hover:text-blue-300"
-            }`}
+            className={`px-4 py-2 rounded-md cursor-pointer transition-all duration-200 ${selectedCategory === cat ? "bg-blue-500 text-white" : "hover:text-blue-300"
+              }`}
           >
             {cat.charAt(0).toUpperCase() + cat.slice(1)}
           </button>
@@ -247,14 +120,14 @@ const Projects = () => {
             />
           </>
         ) : filteredProjects.length === 1 ? (
-          // ✅ Single project (controlled width, centered)
+          //  Single project (controlled width, centered)
           <div className="flex justify-center">
             <div className="max-w-[400px] w-full">
               <ProjectCard {...filteredProjects[0]} />
             </div>
           </div>
         ) : (
-          // ✅ No projects
+          //  No projects
           <p className="text-white text-lg text-center">No projects found in this category.</p>
         )}
       </div>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,16 +1,9 @@
-import Certificate1 from '../assets/images/NxtLogic.jpg'
-import Certificate2 from '../assets/images/frontend.png'
-import Certificate3 from '../assets/images/UIUX.png'
-import Certificate4 from '../assets/images/api skills.png'
-import Certificate5 from '../assets/images/Tech vedha.png'
-import Certificate6 from '../assets/images/FullStack.png'
-import Certificate7 from '../assets/images/CyberSecurity.png'
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import SkillsData from "../Datas/SkillsData.jsx";
 
 import { FaHtml5, FaCss3Alt, FaNodeJs, FaReact } from "react-icons/fa";
 import { SiJavascript, SiTailwindcss, SiExpress } from "react-icons/si";
@@ -30,47 +23,7 @@ const MySkills = [
 
 // Skills data
 
-const data = [
 
-    {
-        name: 'Mern Stack - NoviTech',
-        image: Certificate6,
-        content: 'Completed 30-day intensive training in Full Stack Development with practical, hands-on projects.(Augest –Septemper 2025)'
-    },
-    {
-        name: 'Web Development – Tech Vedhu',
-        image: Certificate5,
-        content: 'Completed an intensive course covering core web development concepts and best practices (June–July 2025).'
-    },
-    {
-        name: 'Nxtlogic Internship',
-        image: Certificate1,
-        content: 'Completed a front-end internship focused on real-time projects and team collaboration.(July 2024)'
-    },
-
-    {
-        name: 'API Development Skills',
-        image: Certificate4,
-        content: 'Gained experience in building and integrating RESTful APIs.'
-    },
-    {
-        name: 'AI Powered Cyber Security Defense',
-        image: Certificate7,
-        content: 'Successfully completed a bootcamp on AI Powered Cyber Security Defense conducted by NoviTech R&D Private Limited (September 13, 2025).'
-    },
-
-    {
-        name: 'UI/UX Design Training',
-        image: Certificate3,
-        content: 'Training in UI/UX design principles and user-centered workflows.(June–July 2025)'
-    },
-    {
-        name: 'Frontend Bootcamp ',
-        image: Certificate2,
-        content: 'Certified for completing a comprehensive front-end development program.'
-    },
-
-];
 
 // Skills left side
 
@@ -117,7 +70,7 @@ const Skills = () => {
                     pagination={{ clickable: true }}
                     className="md:w-[400px] md:h-[350px] md:px-6 w-[280px] h-[350px] "
                 >
-                    {data.map((d, index) => (
+                    {SkillsData.map((d, index) => (
                         <SwiperSlide key={index}>
                             <div className="bg-[#334155] border border-gray-600 md:w-[400px]  p-4 rounded-xl h-full flex flex-col items-center  hover:bg-[#1f2937] transition">
                                 <img src={d.image} alt={d.name} className="w-[300px] h-48 object-fill rounded-lg mb-3 border border-white" />
